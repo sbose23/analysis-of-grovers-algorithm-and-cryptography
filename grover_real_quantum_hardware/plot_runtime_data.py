9b03b59a95001692b9c7c81256e0f8e63fddabd6
Sudipto Bose <sbose@torontomu.ca>
2025-03-29 18:16:24 -0400

    added runtime data of grover on quantum hardware + plot

---
+++ grover_real_quantum_hardware/plot_runtime_data.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import json 
+
+with open('./grover_real_quantum_hardware/runtime_data.json', 'r') as f:
+    runtime_data = json.load(f)
+
+with open('./grover_real_quantum_hardware/runtime_increase_trend.json', 'r') as f:
+    runtime_increase_trend = json.load(f)
+
+x1 = list(map(int, runtime_data.keys()))
+y = list(runtime_data.values())
+
+x2 = list(map(int, runtime_increase_trend.keys()))
+z = list(runtime_increase_trend.values())
+
+fig, ax1 = plt.subplots()
+
+ax1.plot(x1, y, 'bo-', label='Runtime (s)')
+ax1.set_xlabel('Bitstring length')
+ax1.set_ylabel('Runtime (s)', color='b')
+ax1.tick_params(axis='y', labelcolor='b')
+
+ax2 = ax1.twinx()
+ax2.plot(x2, z, 'r^-', label='Runtime increase trend (%)')
+ax2.set_ylabel('Runtime increase trend (%)', color='r')
+ax2.tick_params(axis='y', labelcolor='r')
+
+plt.title("Runtime & Runtime Increase Trend of Grover's Algorithm")
+fig.tight_layout()
+plt.grid(True)
+plt.show()
