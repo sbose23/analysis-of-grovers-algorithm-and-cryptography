32eebc26e09b3010022cd4422109b9edd99f5faf
Sudipto Bose <sbose@torontomu.ca>
2025-03-29 19:25:49 -0400

    updated real hardware chart

--- grover_real_quantum_hardware/plot_runtime_data.py
+++ grover_real_quantum_hardware/plot_runtime_data.py
@@ -21,10 +21,10 @@ ax1.set_ylabel('Runtime (s)', color='b')
 ax1.tick_params(axis='y', labelcolor='b')
 
 ax2 = ax1.twinx()
-ax2.plot(x2, z, 'r^-', label='Runtime increase trend (%)')
-ax2.set_ylabel('Runtime increase trend (%)', color='r')
+ax2.plot(x2, z, 'r^-', label='Percentage increase (%)')
+ax2.set_ylabel('Percentage increase (%)', color='r')
 ax2.tick_params(axis='y', labelcolor='r')
 
-plt.title("Runtime & Runtime Increase Trend of Grover's Algorithm")
+plt.title("Runtime & Runtime Increase Trend of Grover's Algorithm on Real Quantum Hardware")
 fig.tight_layout()
 plt.show()
