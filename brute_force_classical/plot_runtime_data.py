d27ce886006c14e6d7d21b74f201f8878bd3daca
Sudipto Bose <sbose@torontomu.ca>
2025-03-29 19:27:10 -0400

    updated brute force chart

--- brute_force_classical/plot_runtime_data.py
+++ brute_force_classical/plot_runtime_data.py
@@ -21,8 +21,8 @@ ax1.set_ylabel('Runtime (s)', color='b')
 ax1.tick_params(axis='y', labelcolor='b')
 
 ax2 = ax1.twinx()
-ax2.plot(x2, z, 'r^-', label='Runtime increase trend (%)')
-ax2.set_ylabel('Runtime increase trend (%)', color='r')
+ax2.plot(x2, z, 'r^-', label='Percentage increase (%)')
+ax2.set_ylabel('Percentage increase (%)', color='r')
 ax2.tick_params(axis='y', labelcolor='r')
 
 plt.title("Runtime & Runtime Increase Trend of Brute Force AES Key Search")
